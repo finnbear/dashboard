0ce34b509fee9ad3d8bf9ae868513a7b111eac2c
finnbear <finnbearone@gmail.com>
2017-02-07 13:27:40 -0800

    Javascript working.

---
+++ script.js
@@ -0,0 +1,35 @@
+var Game = {}
+var Keyboard = {keys: []}
+var Display = {canvas: null, context: null, width: 0, height: 0, backgroundColor: "white"}
+
+Game.initialize = function() {
+	Display.canvas = document.getElementById("viewport");
+	Display.context = Display.canvas.getContext("2d");
+	Display.width = Display.canvas.width;
+	Display.height = Display.canvas.height;
+}
+
+Game.update = function() {
+
+}
+
+Game.draw = function() {
+	Display.context.clearRect(0, 0, Display.width, Display.height);
+	Display.context.fillStyle = Display.backgroundColor;
+	Display.context.fillRect(0, 0, Display.width, Display.height);
+}
+
+window.addEventListener("keydown",
+    function(e){
+        Keyboard.keys[e.keyCode] = true;
+		if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        	e.preventDefault();
+    	}
+    },
+    false);
+
+window.addEventListener('keyup',
+    function(e){
+        Keyboard.keys[e.keyCode] = false;
+    },
+    false);
